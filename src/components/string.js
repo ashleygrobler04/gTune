541d7aa02eb127c9c53454094035c3832098482f
ashleygrobler04 <ashleygrobler04@gmail.com>
2024-12-03 13:52:35 +0200

    styles

--- src/components/string.js
+++ src/components/string.js
@@ -1,10 +1,19 @@
-import {playNote} from "../audio";
+import React from "react";
+import String from "./components/string";
+import { stringFrequencies } from "./audio";
+import "./string.css";
 
-const String = (props) => {
-  const handleClick = () => {
-    //console.log("Hi there");
-    playNote(props.freq);
-  };
-  return <button onClick={handleClick}>{props.name}</button>;
-};
-export default String;
+function App() {
+  return (
+    <div className="container">
+      <String name="String 1" freq={stringFrequencies.e4} />
+      <String name="String 2" freq={stringFrequencies.B3} />
+      <String name="String 3" freq={stringFrequencies.G3} />
+      <String name="String 4" freq={stringFrequencies.D3} />
+      <String name="String 5" freq={stringFrequencies.A2} />
+      <String name="String 6" freq={stringFrequencies.E2} />
+    </div>
+  );
+}
+
+export default App;
